e9a06db8aecfa8397682992a62edde05097d3b22
Penelope Yong <penelopeysm@gmail.com>
2025-07-01 23:35:52 +0100

    fix?

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { base } from '$app/paths';
+    let { children } = $props();
+</script>
+
+<svelte:head>
+    <title>Talks</title>
+    <link rel="stylesheet" href="{base}/main.css" />
+</svelte:head>
+
+{@render children()}
