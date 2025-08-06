4642b5d9e5a62d6af1cef4c91e71ba9e4bce62a8
Penelope Yong <penelopeysm@gmail.com>
2025-08-06 10:45:39 +0100

    Add copy button to code examples

--- src/lib/CodeExample.svelte
+++ src/lib/CodeExample.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
 	import Highlight from 'svelte-highlight';
-    import type { LanguageType } from 'svelte-highlight/languages';
+	import type { LanguageType } from 'svelte-highlight/languages';
 	interface CodeExampleProps {
 		anchorname: string;
 		language: LanguageType<string>;
 		filename: string;
 		code: string;
-        description: any;
+		description: any;
 	}
+	import { copy } from 'svelte-copy';
 	const { anchorname, language, filename, code, description }: CodeExampleProps = $props();
+
+	const trimmedCode = code.trim();
+	let buttonText = $state('Copy');
+	function onCopy() {
+		buttonText = 'Copied!';
+		setTimeout(() => {
+			buttonText = 'Copy';
+		}, 2000);
+	}
 </script>
 
 <h2 id={anchorname}>{filename}</h2>
@@ -16,4 +26,36 @@
 
 {@render description()}
 
-<Highlight {language} code={code.trim()} />
+<div class="code-example">
+	<Highlight {language} code={trimmedCode} />
+	<button
+		class="copy"
+		use:copy={{
+			text: trimmedCode,
+			onCopy
+		}}>{buttonText}</button
+	>
+</div>
+
+<style>
+	.code-example {
+		position: relative;
+	}
+
+	button.copy {
+		font-family: inherit;
+		padding: 4px 8px;
+		border: 1.5px solid black;
+		border-radius: 5px;
+		cursor: pointer;
+		transition: background-color 0.3s ease;
+		position: absolute;
+		top: 10px;
+		/* the <pre> class has extra 50px margin so we adjust for that */
+		right: 70px;
+		font-size: 0.8em;
+	}
+	button.copy:hover {
+		background-color: #f3f3f3;
+	}
+</style>
